777de766aed99f4dcac6fe3f8171bc5f1a40cf4a
Sebastian Smiley <sebastianswms@gmail.com>
2024-11-12 14:08:01 -0500

    dynamic URIs, better input examples

--- target_apprise/sinks.py
+++ target_apprise/sinks.py
@@ -1,5 +1,6 @@
 """Apprise target sink class, which handles writing streams."""
-from typing import Optional
+import re
+import typing as t
 
 import apprise
 from singer_sdk.sinks import RecordSink
@@ -8,15 +9,48 @@ from singer_sdk.sinks import RecordSink
 class AppriseSink(RecordSink):
     """Apprise target sink class."""
 
-    def process_record(self, record: dict, context: dict) -> None:
+    def process_record(self, record: dict[str, t.Any], context: dict) -> None:
         """Process the record."""
         a = apprise.Apprise()
         for uri in self.config["uris"]:
-            a.add(uri)
+            formatted_uri: str = uri
+            if self.config["uri_replacement"]:
+                for k, v in record.items():
+                    if not k.startswith("_sdc_replace_"):
+                        continue
+                    to_replace = f"{{{k}}}" # Ex: '{_sdc_replace_target_email}'
 
-        title: Optional[str] = record.get("title")
-        body: Optional[str] = record.get("body")
-        attach: Optional[str] = record.get("attach")
+                    if to_replace not in uri:
+                        debug_msg = (  # Contains secrets!
+                            "Encountered an error while attempting to do URI "
+                            f"replacement for URI: '{uri}'."
+                        )
+                        self.logger.debug(debug_msg)
+                        msg = (
+                            f"The uri_replacement column of '{k}' exists but at least "
+                            "one of the configured `uris` is missing it."
+                        )
+                        raise RuntimeError(msg)
+
+                    formatted_uri = formatted_uri.replace(to_replace, v)
+
+                pattern = r"\{_sdc_replace_.*?\}"
+                if re.search(pattern, formatted_uri):
+                    debug_msg = (  # Contains secrets!
+                        "Encountered an error while attempting to do URI "
+                        f"replacement for URI: '{uri}'."
+                    )
+                    self.logger.debug(debug_msg)
+                    msg = (
+                        "At least one of the configured `uris` contains "
+                        "uri_replacement placeholders for which no matching "
+                        "columns exist."
+                    )
+            a.add(formatted_uri)
+
+        title: t.Optional[str] = record.get("title")
+        body: t.Optional[str] = record.get("body")
+        attach: t.Optional[str] = record.get("attach")
         if title is None and body is None:
             raise Exception("Both the title and body cannot be None")
         if a.notify(title=title, body=body, attach=attach) is False:
